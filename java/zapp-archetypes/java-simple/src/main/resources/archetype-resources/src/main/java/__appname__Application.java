b80812facd71b85ec7f0704808ff718658f12888
smousa <smousa@zenoss.com>
2013-06-20 11:32:51 -0500

    Created archetype for simple zenoss app

---
+++ java/zapp-archetypes/java-simple/src/main/resources/archetype-resources/src/main/java/__appname__Application.java
@@ -0,0 +1,27 @@
+/*
+ * ****************************************************************************
+ *
+ *  Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+ *
+ *  This content is made available according to terms specified in
+ *  License.zenoss under the directory where your Zenoss product is installed.
+ *
+ * ***************************************************************************
+ */
+
+package ${package};
+
+import org.zenoss.app.AutowiredApp;
+
+public class ${appname}Application extends AutowiredApp<${appname}Configuration> {
+	
+	public static void main(String[] args) throws Exception {
+		new ${appname}Application().run(args);
+	}
+	
+	@Override
+	public String getName() {
+		return "${appname}"
+	}
+	
+}
---
+++ java/zapp-archetypes/java-simple/src/main/resources/archetype-resources/src/main/java/api/__apiname__/__apiname__Response.java
@@ -0,0 +1,16 @@
+/*
+ * ****************************************************************************
+ *
+ *  Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+ *
+ *  This content is made available according to terms specified in
+ *  License.zenoss under the directory where your Zenoss product is installed.
+ *
+ * ***************************************************************************
+ */
+
+package ${package}.api.${apiname};
+
+public final class ${apiname}Response {
+	/* TODO: Initialize POJO */
+}
