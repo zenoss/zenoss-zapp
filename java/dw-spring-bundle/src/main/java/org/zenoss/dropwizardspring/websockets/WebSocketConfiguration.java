d7b2f8e29928c587f9963ea72ca8df45e46fe8ce
Seth Cleveland <seth.cleveland@gmail.com>
2014-03-04 13:54:10 -0600

    websocket config object comments

--- java/dw-spring-bundle/src/main/java/org/zenoss/dropwizardspring/websockets/WebSocketConfiguration.java
+++ java/dw-spring-bundle/src/main/java/org/zenoss/dropwizardspring/websockets/WebSocketConfiguration.java
@@ -3,27 +3,51 @@ package org.zenoss.dropwizardspring.websockets;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class WebSocketConfiguration {
+    /**
+     * executor service maximum threads for broadcasting messages
+     */
     @JsonProperty
     private int maxBroadcastThreads = 10;
 
+    /**
+     * executor service minumum threads for broadcasting messages
+     */
     @JsonProperty
     private int minBroadcastThreads = 5;
 
+    /**
+     * executor service threads keep alive time
+     */
     @JsonProperty
     private int broadcastThreadKeepAliveMillis = 60000;
 
+    /**
+     * used to set the time in ms that a websocket may be idle before closing.
+     */
     @JsonProperty
     private Integer maxIdleTime;
 
+    /**
+     * used to set the size in bytes that a websocket may be accept before closing.
+     */
     @JsonProperty
     private Integer maxBinaryMessageSize;
 
+    /**
+     * used to set the size in characters that a websocket may be accept before closing.
+     */
     @JsonProperty
     private Integer maxTextMessageSize;
 
+    /**
+     * used to set the buffer size, which is also the max frame byte size (default 8192).
+     */
     @JsonProperty
     private Integer bufferSize;
 
+    /**
+     * used to set the minimum protocol version accepted. Default is the RFC6455 version (13)
+     */
     @JsonProperty
     private String minVersion;
 
